2238a6c91957bb5a9f489ea71bfd611277bed40c
Josiah <josiah_g@live.com>
2024-05-20 10:21:44 +0200

    Finished investment calculator

--- investment-calculator/investment_calculator.go
+++ investment-calculator/investment_calculator.go
@@ -7,13 +7,38 @@ import (
 )
 
 func main() {
-	var investmentAmount, years float64 = 1000 , 10
-	var expectedReturn = 5.5
-	// var years float64 = 10
-	// When type should be inferred, use := instead of var
-    example := "Hello, World!"
+	var inflationRate float64
+	var investmentAmount float64
+	var years float64
+	var expectedReturn float64
+	// := both declares and initializes a variable.
+	// = only assigns a value to an already declared variable.
+	// Scope:
 
-	var investmentReturn = investmentAmount * math.Pow(1+expectedReturn/100, years)
-	fmt.Print("Return after ", years, " years is: $", math.Round(investmentReturn*100)/100, "\n")
-	fmt.Println(example)
+	// := can only be used inside functions.
+	// = can be used inside and outside functions.
+	// Multiple Variables:
+
+	// := can declare and initialize multiple variables in one line.
+	// = assigns values to already declared variables.
+
+	//*****************************************************************************
+	fmt.Print("Enter the investment amount: ")
+	fmt.Scan(&investmentAmount)
+	// asking for expected return rate
+	fmt.Print("Enter the expected return rate: ")
+	fmt.Scan(&expectedReturn)
+	// asking for number of years
+	fmt.Print("Enter the number of years: ")
+	fmt.Scan(&years)
+
+	// Asking for inflation rate
+	fmt.Print("Enter the inflation rate: ")
+	fmt.Scan(&inflationRate)
+
+	// Calculating the investment return and inflation adjusted return then printing the result
+	investmentReturn := investmentAmount * math.Pow(1+expectedReturn/100, years)
+	inflationAdjustedReturn := investmentReturn / math.Pow(1+inflationRate/100, years)
+	fmt.Println("Return after ", years, " years is: $", math.Round(investmentReturn*100)/100)
+	fmt.Println(inflationAdjustedReturn)
 }
